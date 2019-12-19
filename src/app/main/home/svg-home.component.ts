9d2f8b5ed977f2e89d5938c07a187e1f57d795c1
Chinweike David <45151879+daveOnactive@users.noreply.github.com>
2019-12-20 00:03:05 +0100

    Update svg home page

--- src/app/main/home/svg-home.component.ts
+++ src/app/main/home/svg-home.component.ts
@@ -16,8 +16,8 @@ export class SvgHomeComponent implements AfterViewInit {
       targets: '#svg path',
       strokeDashoffset: [anime.setDashoffset, 0],
       easing: 'easeInOutSine',
-      duration: 500,
-      delay: (el, i) => { return i * 500 }
+      duration: 400,
+      delay: (el, i) => { return i * 400 }
     });
   }
 
