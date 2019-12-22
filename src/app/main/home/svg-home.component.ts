2afc01bffdfe2242a4c4ef190a674c37cb194b12
Chinweike David <45151879+daveOnactive@users.noreply.github.com>
2019-12-22 13:18:44 +0100

    Update svg-home.component.ts

--- src/app/main/home/svg-home.component.ts
+++ src/app/main/home/svg-home.component.ts
@@ -17,7 +17,7 @@ export class SvgHomeComponent implements AfterViewInit {
       strokeDashoffset: [anime.setDashoffset, 0],
       easing: 'easeInOutSine',
       duration: 200,
-      delay: (el, i) => { return i * 200 }
+      delay: (el, i) => { return i * 150 }
     });
   }
 
